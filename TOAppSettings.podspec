b3506f4b3132927bc5bc52fa0ce287e126cb2b7b
Quintz Gábor <quiga@digitus.itk.ppke.hu>
2019-04-17 14:02:07 +0200

    Default value bug fix: Check the NSUserDefaults contains the key of default value instead of check the prefix.

--- TOAppSettings.podspec
+++ TOAppSettings.podspec
@@ -6,7 +6,7 @@ Pod::Spec.new do |s|
   s.homepage = 'https://github.com/TimOliver/TOAppSettings'
   s.author   = 'Tim Oliver'
   s.source   = { :git => 'https://github.com/TimOliver/TOAppSettings.git', :tag => s.version }
-  s.platform = :ios, '9.0'
+  s.platform = :ios, '8.0'
   s.source_files = 'TOAppSettings/**/*.{h,m}'
   s.requires_arc = true
 end
--- TOAppSettingsTests/TOAppSettingsTests.m
+++ TOAppSettingsTests/TOAppSettingsTests.m
@@ -32,7 +32,7 @@
 
 + (NSDictionary *)defaultPropertyValues
 {
-    return @{@"isDrunk": @YES};
+	return @{@"isDrunk": @YES, @"height": @(-1.05f)};
 }
 
 @end
@@ -81,6 +81,39 @@
     XCTAssert([settings.voiceActors[@"English"] isEqualToString:@"Justin Roiland"]);
     XCTAssert([[[NSString alloc] initWithData:settings.portalGunFormula encoding:NSUTF8StringEncoding] isEqualToString:@"McDonald's Szechuan McNugget Sauce"]);
     XCTAssert([settings.portalGunColor isEqual:[UIColor colorWithRed:0.0f green:1.0f blue:0.0f alpha:1.0f]]);
+}
+
+- (void)testDefaultValueUpdate {
+	
+	NSUserDefaults * defaults = [NSUserDefaults standardUserDefaults];
+	
+	// note: "className.ID.property"
+	
+	NSString * clsName = @"TestSettings";
+	NSString * identifier = @"DefValTest";
+
+	[defaults setObject:@"default-value"
+				 forKey:[NSString stringWithFormat:@"%@.%@.%@", clsName, identifier, @"Name"]];
+
+	[defaults setObject:@(42)
+				 forKey:[NSString stringWithFormat:@"%@.%@.%@", clsName, identifier, @"Age"]];
+
+	[defaults synchronize];
+	
+	// Set initial object, then dealloc
+	@autoreleasepool {
+		TestSettings * settings = [TestSettings settingsWithIdentifier:identifier];
+		settings = nil;
+	}
+	
+	// Get a second copy of the same instance and compare data was successfully retrieved
+	TestSettings * settings = [TestSettings settingsWithIdentifier:identifier];
+	
+	XCTAssert([settings.name isEqualToString:@"default-value"]);
+	XCTAssert(settings.age == 42);
+	
+	XCTAssert(settings.height == -1.05f);
+	XCTAssert(settings.isDrunk == YES);
 }
 
 // Test that two separate copies of the same object can be saved
